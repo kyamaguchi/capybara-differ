8476c41c048105ef01af6781d4f703363bb33053
Kazuho Yamaguchi <kzh.yap@gmail.com>
2018-04-04 17:55:02 +0900

    Add the information of selector to log

--- lib/capybara/refactoring.rb
+++ lib/capybara/refactoring.rb
@@ -19,7 +19,7 @@ module Capybara
           puts "There is no history of snapshots"
           return ''
         end
-        puts "Comparing two files\n  #{@old_file_path}\n  #{@new_file_path}"
+        puts "Comparing two files#{target_selector ? ' with selector [' + target_selector + ']' : ''}\n  #{@old_file_path}\n  #{@new_file_path}"
         old_html = beautified_html(@old_file_path)
         new_html = beautified_html(@new_file_path)
         Diffy::Diff.new(old_html, new_html, context: 2).to_s(:color)
